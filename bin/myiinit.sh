1ca6c40d52f55e50a496773733c422c790854d21
Helen Cousins <hc7@sanger.ac.uk>
2018-05-14 13:10:46 +0100

    update

--- bin/myiinit.sh
+++ bin/myiinit.sh
@@ -1,5 +1,6 @@
-export IRODS_USER_NAME=$(cat /secret/IRODS_USER_NAME)
-export IRODS_PASSWORD=$(cat /secret/IRODS_PASSWORD)
-sed -i "s/USER/$IRODS_USER_NAME/" /root/.irods/irods_environment.json
-echo $IRODS_PASSWORD | iinit
+#!/bin/bash -e
 
+sed -i "s/USER/$(cat /secret/IRODS_USER_NAME)/" /root/.irods/irods_environment.$
+echo $(cat /secret/IRODS_PASSWORD) | iinit
+
+rm -r /secret
